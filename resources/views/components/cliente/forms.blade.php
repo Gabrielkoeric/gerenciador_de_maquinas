312b9846c2ffa06d4f39f295945413b4ae8c7a2f
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2025-04-28 21:52:41 -0300

    ajuste rotina de cliente

---
+++ resources/views/components/cliente/forms.blade.php
@@ -0,0 +1,30 @@
+<form action="{{$action}}" method="post">
+    @csrf
+    @isset($nome)
+        @method('PUT')
+    @endisset
+
+    <div class="mb-3">
+        <label for="nome" class=form-label>Nome:</label>
+        <input type="text" id="nome" name="nome" class="form-control" @isset($nome) value="{{$nome}}" @endisset required>
+
+        <label for="sigla" class=form-label>Sigla</label>
+        <input type="text" id="sigla" name="sigla" class="form-control" @isset($sigla) value="{{$sigla}}" @endisset required>
+
+        <label for="licenca" class=form-label>Licenca</label>
+        <input type="text" id="licenca" name="licenca" class="form-control" @isset($licenca) value="{{$licenca}}" @endisset required>
+
+        <label for="coletor" class=form-label>Coletor</label>
+        <input type="text" id="coletor" name="coletor" class="form-control" @isset($coletor) value="{{$coletor}}" @endisset required>
+
+        <label for="desktop" class=form-label>Desktop</label>
+        <input type="text" id="desktop" name="desktop" class="form-control" @isset($desktop) value="{{$desktop}}" @endisset required>
+
+        <label for="ativo" class=form-label>Ativo:</label><br>
+        <input type="checkbox" class="form-control-input" id="ativo" name="ativo" value="1"
+        @isset($ativo) @if($ativo) checked @endif @else checked @endisset>
+
+    </div>
+    <button type="submit" class="btn btn-primary">Salvar</button>
+    <a href="{{route('cliente_escala.index')}}" class="btn btn-primary">Cancelar</a>
+</form>
