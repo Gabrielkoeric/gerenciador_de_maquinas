09b6fdf7ce1202cbd3072343ae763a8a567dc5a8
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2026-02-04 23:27:32 -0300

    ajuste coleta info de usuarios logados no rdp

---
+++ scriptyAnsible/vm/finalizardesconectado.ps1
@@ -0,0 +1,4 @@
+﻿param(
+    [string]$nomevm
+)
+get-RDUserSession | where  SessionState -Like 'STATE_DISCONNECTED' | foreach { Invoke-RDUserLogoff -HostServer $nomevm -UnifiedSessionID $_.UnifiedSessionId -Force}
