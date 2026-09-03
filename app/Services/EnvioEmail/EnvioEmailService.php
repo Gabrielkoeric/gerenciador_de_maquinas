3a6b3d21e912a6fe4d73b8f3b3cec57bccfdaa00
Gabrielkoeric <gabriel.koerich@unidavi.edu.br>
2026-09-03 13:51:20 -0300

    service de envio de email

--- app/Services/EnvioEmail/EnvioEmailService.php
+++ app/Services/EnvioEmail/EnvioEmailService.php
@@ -11,42 +11,79 @@ class EnvioEmailService
     {
         try {
 
-            // 🔥 Se tiver config do usuário, sobrescreve SMTP
             if ($config) {
+
+                $mailerName = 'smtp_usuario_' .
+                    $config->id_usuario_email_config . '_' .
+                    uniqid();
+
                 config([
-                    'mail.mailers.smtp.host' => $config->host,
-                    'mail.mailers.smtp.port' => $config->port,
-                    'mail.mailers.smtp.username' => $config->username,
-                    'mail.mailers.smtp.password' => $config->password ? decrypt($config->password) : null,
-                    'mail.mailers.smtp.encryption' => $config->criptografia,
-
-                    'mail.from.address' => $config->from_address,
-                    'mail.from.name' => $config->from_name,
+                    "mail.mailers.{$mailerName}" => [
+                        'transport' => 'smtp',
+                        'host' => $config->host,
+                        'port' => $config->port,
+                        'username' => $config->username,
+                        'password' => $config->password
+                            ? decrypt($config->password)
+                            : null,
+                        'encryption' => $config->criptografia,
+                    ],
                 ]);
+
+                Mail::mailer($mailerName)->raw(
+                    $mensagem,
+                    function ($message) use (
+                        $emailDestino,
+                        $assunto,
+                        $anexo,
+                        $config
+                    ) {
+
+                        $message->to($emailDestino)
+                            ->subject($assunto);
+
+                        if ($config->from_address) {
+                            $message->from(
+                                $config->from_address,
+                                $config->from_name
+                            );
+                        }
+
+                        if ($anexo && file_exists($anexo)) {
+                            $message->attach($anexo);
+                        }
+                    }
+                );
+
+            } else {
+
+                Mail::raw(
+                    $mensagem,
+                    function ($message) use (
+                        $emailDestino,
+                        $assunto,
+                        $anexo
+                    ) {
+
+                        $message->to($emailDestino)
+                            ->subject($assunto);
+
+                        if ($anexo && file_exists($anexo)) {
+                            $message->attach($anexo);
+                        }
+                    }
+                );
             }
-
-            Mail::raw($mensagem, function ($message) use ($emailDestino, $assunto, $anexo, $config) {
-
-                $message->to($emailDestino)
-                        ->subject($assunto);
-
-                // 🔥 garante o from correto
-                if ($config && $config->from_address) {
-                    $message->from($config->from_address, $config->from_name);
-                }
-
-                if ($anexo && file_exists($anexo)) {
-                    $message->attach($anexo);
-                }
-            });
 
             Log::info("Email enviado com sucesso para {$emailDestino}");
 
         } catch (\Throwable $e) {
 
-            Log::error("Erro ao enviar email para {$emailDestino}: " . $e->getMessage());
+            Log::error(
+                "Erro ao enviar email para {$emailDestino}: " .
+                $e->getMessage()
+            );
 
-            // importante pra queue retry
             throw $e;
         }
     }
